a9df1305845aee1996507ef72f28c5ce39b96509
Tobijah96 <127013960+Tobijah96@users.noreply.github.com>
2024-05-06 18:37:40 +0200

    Update variables.tf

---
+++ azure-blob-storage/variables.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  description = "Nazwa grupy zasobów"
+}
+
+variable "storage_account_name" {
+  description = "Nazwa konta magazynu"
+}
+
+variable "container_name" {
+  description = "Nazwa kontenera"
+}
+
+variable "blob_name" {
+  description = "Nazwa bloba"
+}
+
+variable "location" {
+  description = "Lokalizacja zasobów"
+}
+
+variable "storage_account_tier" {
+  description = "Typ konta magazynu"
+}
